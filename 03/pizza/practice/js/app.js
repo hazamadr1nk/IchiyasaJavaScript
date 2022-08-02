e6b83336eb3cb88ab64169bbe0f3ea869b8a2207
hazamadr1nk <110225244+hazamadr1nk@users.noreply.github.com>
2022-08-02 21:33:38 +0900

    Lesson21を実装

--- 03/pizza/practice/js/app.js
+++ 03/pizza/practice/js/app.js
@@ -2,4 +2,8 @@ let budget = prompt("所持金を数字で入力して下さい");
 budget = parseFloat(budget);
 if (budget >= 1500) {
     alert("ピザを注文した");
+} else if (budget >= 500) {
+    alert("ポテトを注文しました");
+} else {
+    alert("節約、節約・・・");
 }
