1096cb4db8670b3051b9be0b4d42a88f4ae40dbc
hazamadr1nk <110225244+hazamadr1nk@users.noreply.github.com>
2022-08-03 14:10:31 +0900

    Lesson 44 を実装

--- 07/form/practice/js/app.js
+++ 07/form/practice/js/app.js
@@ -2,6 +2,16 @@
 // フォームの要素を取得
 let button = document.getElementById("button");
 let form = document.getElementById("form");
+let textarea = document.getElementById("textarea");
+
+//　文字数制限
+let maxTextNum = textarea.getAttribute("maxlength");
+
+// 要素の追加
+// 残り文字数を表示する要素の追加
+let textMessage = document.createElement("div");
+let parent = textarea.parentElement;
+parent.insertBefore(textMessage, textarea);
 
 // イベント処理
 //　お問い合わせボタンを押したとき
@@ -10,3 +20,10 @@ button.addEventListener("click", function()
     //　フォームを表示
     form.style.display = "block";
 })
+
+// テキストエリアでキーをタイプしたとき
+textarea.addEventListener("keyup", function() 
+{
+    let currentTextNum = textarea.value.length;
+    textMessage.innerHTML = "<p>あと「" + (maxTextNum - currentTextNum) + "」文字入力できます。</p>"
+});
